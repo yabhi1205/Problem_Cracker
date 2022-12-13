13585b1358f6a203bb8d72ee2ab0dd3668885de4
Abhimrt <abhisinghal112001@gmail.com>
2022-12-13 11:44:57 +0530

    styling home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import  './components/scss/style.css';
+import './components/scss/style.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/Blogin/Main';
 
@@ -8,10 +8,46 @@ function App() {
 
   return (
     <div className="App">
+        {/* <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar scroll</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarScroll">
+              <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Link
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><hr className="dropdown-divider"/></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled">Link</a>
+                </li>
+              </ul>
+              <div className="d-flex">
+                
+              </div>
+            </div>
+          </div>
+        </nav> */}
+
       <Routes>
-        <Route path='/' element={<Main/>}/>
+        <Route path='/' element={<Main />} />
       </Routes>
-      
+
     </div>
   );
 }
--- src/components/Blogin/Main.js
+++ src/components/Blogin/Main.js
@@ -16,11 +16,11 @@ const Main = () => {
             <div className=" main_home">
                 <div className="row ">
                     <div className="col">
-                        <img src={require("../images/home.png")} alt="" />
+                        <img src={require("../images/home.png")} className="home_img" alt="" />
                     </div>
                     <div className="col">
                         <div className="shape_home"></div>
-                        <button onClick={handleClick} className="btn btn-primary">Google signin</button>
+                        <button onClick={handleClick} className="btn "> <i class="fa-brands fa-google"></i><span>Google signin</span></button>
                     </div>
                 </div>
             </div>
